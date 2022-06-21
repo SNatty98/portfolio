afabf621034d50a1d0682fdfe1e9942f03c6f9b5
SNatty98 <mrnatt98@gmail.com>
2022-06-21 14:02:54 +0100

    Header component updated

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+      <a href="https://www.linkedin.com/in/sean-natty-37119a195/" target="_blank"></a>
+      <a href="https://www.github.com" target="_blank"></a>
+    </div>
+  );
+};
+
+export default HeaderSocials;
