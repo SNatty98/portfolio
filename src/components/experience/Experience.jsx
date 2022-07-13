e9e0b22eb0fdb2f662ee1b752609e61ea1b1bafd
SNatty98 <mrnatt98@gmail.com>
2022-07-13 01:25:27 +0100

    experience component complete

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -11,70 +11,94 @@ const Experience = () => {
           <h3>Frontend Development</h3>
           <div className="experience__content">
             <article className="experience__details">
-              <BsFillPatchCheckFill />
-              <h4>HTML</h4>
-              <small className="text-light">Experienced</small>
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experienced</small>
+              </div>
             </article>
             <article className="experience__details">
-              <BsFillPatchCheckFill />
-              <h4>CSS</h4>
-              <small className="text-light">Intermediate</small>
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>CSS</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
             </article>
             <article className="experience__details">
-              <BsFillPatchCheckFill />
-              <h4>JSX</h4>
-              <small className="text-light">Intermediate</small>
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>JSX</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
             </article>
             <article className="experience__details">
-              <BsFillPatchCheckFill />
-              <h4>React</h4>
-              <small className="text-light">Intermediate</small>
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>React</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
             </article>
             <article className="experience__details">
-              <BsFillPatchCheckFill />
-              <h4>Bootstrap</h4>
-              <small className="text-light">Intermediate</small>
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Bootstrap</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
             </article>
             <article className="experience__details">
-              <BsFillPatchCheckFill />
-              <h4>Tailwind</h4>
-              <small className="text-light">Beginner</small>
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Tailwind</h4>
+                <small className="text-light">Beginner</small>
+              </div>
             </article>
           </div>
         </div>
-        
+
         <div className="experience__backend">
           <h3>Backend Development</h3>
           <div className="experience__content">
             <article className="experience__details">
-              <BsFillPatchCheckFill />
-              <h4>NodeJS</h4>
-              <small className="text-light">Intermediate</small>
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>NodeJS</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
             </article>
             <article className="experience__details">
-              <BsFillPatchCheckFill />
-              <h4>Axios</h4>
-              <small className="text-light">Intermediate</small>
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Axios</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
             </article>
             <article className="experience__details">
-              <BsFillPatchCheckFill />
-              <h4>JSX</h4>
-              <small className="text-light">Intermediate</small>
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>JSX</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
             </article>
             <article className="experience__details">
-              <BsFillPatchCheckFill />
-              <h4>PHP</h4>
-              <small className="text-light">Basic</small>
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>PHP</h4>
+                <small className="text-light">Basic</small>
+              </div>
             </article>
             <article className="experience__details">
-              <BsFillPatchCheckFill />
-              <h4>JUnit Testing</h4>
-              <small className="text-light">Intermediate</small>
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>JUnit Testing</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
             </article>
             <article className="experience__details">
-              <BsFillPatchCheckFill />
-              <h4>MySQL</h4>
-              <small className="text-light">Basic</small>
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>MySQL</h4>
+                <small className="text-light">Basic</small>
+              </div>
             </article>
           </div>
         </div>
