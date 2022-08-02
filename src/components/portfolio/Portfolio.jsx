cdd304760f841d5c22baed24ab0b9ed711eca626
SNatty98 <mrnatt98@gmail.com>
2022-08-02 23:44:28 +0100

    Portfolio section complete

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,7 @@
 import "./portfolio.css";
-import FYPImg from "../../assets/FYPImg.jpg";
-import CalcImg from "../../assets/CalcImg.jpg";
-import EcomImg from "../../assets/EcomImg.jpg";
-
-const Portfolio = () => {
+import FYPImg from "../../assets/FYPImg.png";
+import CalcImg from "../../assets/CalcImg.png";
+import BlogImg from "../../assets/BlogImg.png";
 
 const portfolioData = [
   {
@@ -11,18 +9,22 @@ const portfolioData = [
     image: FYPImg,
     title: "My Final Year Project",
     github: "https://github.com/SNatty98/finalproject",
-    demo: ""
   },
   {
-    id: 1,
-    image: FYPImg,
-    title: "My Final Year Project",
-    github: "https://github.com/SNatty98/finalproject",
-    demo: ""
+    id: 2,
+    image: CalcImg,
+    title: "Calculator",
+    github: "https://github.com/SNatty98/Calculator",
   },
+  {
+    id: 3,
+    image: BlogImg,
+    title: "Blog",
+    github: "https://github.com/SNatty98/react-blog",
+  },
+];
 
-]
-
+const Portfolio = () => {
   return (
     <section id="portfolio">
       <h5>My Recent Work</h5>
@@ -30,74 +32,26 @@ const portfolioData = [
 
       <div className="container">
         <div className="portfolio__container">
-          <article className="portfolio__item">
-            <div className="portfolio__item-image">
-              <img className="portfolio__image" src={FYPImg} alt="" />
-            </div>
-            <h3>Portfolio item title</h3>
-            <div className="portfolio__item-cta">
-              <a
-                href="https://github.com/SNatty98/Calculator"
-                className="btn"
-                target="_blank"
-              >
-                GitHub
-              </a>
-              <a
-                href="https://github.com/SNatty98/Calculator"
-                className="btn btn-primary"
-                target="_blank"
-              >
-                Live Demo
-              </a>
-            </div>
-          </article>
-
-          <article className="portfolio__item">
-            <div className="portfolio__item-image">
-              <img className="portfolio__image" src={FYPImg} alt="" />
-            </div>
-            <h3>Portfolio item title</h3>
-            <div className="portfolio__item-cta">
-              <a
-                href="https://github.com/SNatty98/Calculator"
-                className="btn"
-                target="_blank"
-              >
-                GitHub
-              </a>
-              <a
-                href="https://github.com/SNatty98/Calculator"
-                className="btn btn-primary"
-                target="_blank"
-              >
-                Live Demo
-              </a>
-            </div>
-          </article>
-
-          <article className="portfolio__item">
-            <div className="portfolio__item-image">
-              <img className="portfolio__image" src={FYPImg} alt="" />
-            </div>
-            <h3>Portfolio item title</h3>
-            <div className="portfolio__item-cta">
-              <a
-                href="https://github.com/SNatty98/Calculator"
-                className="btn"
-                target="_blank"
-              >
-                GitHub
-              </a>
-              <a
-                href="https://github.com/SNatty98/Calculator"
-                className="btn btn-primary"
-                target="_blank"
-              >
-                Live Demo
-              </a>
-            </div>
-          </article>
+          {portfolioData.map(({ id, image, title, github }) => {
+            return (
+              <article key={id} className="portfolio__item">
+                <div className="portfolio__item-image">
+                  <img className="portfolio__image" src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item-cta">
+                  <a
+                    href={github}
+                    className="btn"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    GitHub
+                  </a>
+                </div>
+              </article>
+            );
+          })}
         </div>
       </div>
     </section>
